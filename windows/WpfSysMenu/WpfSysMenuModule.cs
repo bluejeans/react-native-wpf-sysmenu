252baadf33f217911fecbcc7c710d0a9b7b29c27
Alexander Bodalevsky <abodalevsky@hotmail.com>
2017-05-10 14:56:25 +0300

    Add RNW package

---
+++ windows/WpfSysMenu/WpfSysMenuModule.cs
@@ -0,0 +1,45 @@
+﻿using ReactNative.Bridge;
+
+namespace WpfSysMenu
+{
+    public sealed class WpfSysMenuModule : ReactContextNativeModuleBase, ILifecycleEventListener
+    {
+        public WpfSysMenuModule(ReactContext reactContext)
+            : base(reactContext)
+        {
+        }
+
+        #region INativeModule Members
+        /// <summary>
+        /// The name of the module.
+        /// </summary>
+        /// <remarks>
+        /// This will be the name used to <code>require()</code> this module
+        /// from JavaScript.
+        /// </remarks>
+        public override string Name => "WpfSysMenuComponent";
+
+        /// <summary>
+        /// Called when a <see cref="IReactInstance"/> is initializing.
+        /// </summary>
+        public override void Initialize()
+        {
+            Context.AddLifecycleEventListener(this);
+        }
+        #endregion
+
+        #region ILifecycleEventListener Members
+        public void OnDestroy()
+        {
+        }
+
+        public void OnResume()
+        {
+        }
+
+        public void OnSuspend()
+        {
+        }
+        #endregion
+    }
+}
---
+++ windows/WpfSysMenu/WpfSysMenuPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace WpfSysMenu
+{
+    public sealed class WpfSusMenuPackage : IReactPackage
+    {
+        #region IReactPackage Members
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return new List<Type>(0);
+        }
+
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new WpfSysMenuModule(reactContext)
+            };
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>(0); //TODO: check for place to initialize menu
+        }
+        #endregion
+    }
+}
